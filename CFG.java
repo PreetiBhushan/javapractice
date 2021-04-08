5d3de51a902a0183fb896aa54ec3c6f18d28cc96
PreetiBhushan <81755026+PreetiBhushan@users.noreply.github.com>
2021-04-08 22:36:50 +0530

    Create CFG.java
    The practice of user-defined method calling.

---
+++ CFG.java
@@ -0,0 +1,8 @@
+public class CFG {
+    public static void main(String[] args){
+        Addition add = new Addition();
+        int sum = add.addTwoInt(1,2); 
+        System.out.println("Sum of two integer values :"+ sum); 
+    }
+    
+}
