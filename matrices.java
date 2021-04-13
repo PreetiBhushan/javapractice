3a88e8848480d15c22d10421814d5a31babacfb2
PreetiBhushan <81755026+PreetiBhushan@users.noreply.github.com>
2021-04-13 15:35:17 +0530

    Update matrices.java
    java in a single row

--- matrices.java
+++ matrices.java
@@ -1,12 +1,16 @@
 public class matrices {
   public static void main(String[] args){
-    int arr1 [] [] = {{1,3,4},{2,4,3},{3,4,5}}; 
-    int arr2 [] [] = {{1,3,4},{2,4,3},{1,2,4}};
+    int arr1 [] [] = {{1,3,4},{2,4,3},{3,4,5}};
     for (int m = 0; m <3; m++){
        for (int n = 0; n <3; n++){
          System.out.print (arr1 [m][n] +" ");
-          System.out.print (arr2 [m][n] +" ");
        }
+       }
+  int arr2 [] [] = {{1,3,4},{2,4,3},{1,2,4}};
+    for (int i= 0; i <3; i++){
+       for (int j = 0; j <3; j++){
+         System.out.print (arr1 [i][j] +" ");
     }
   }
+}
 }
